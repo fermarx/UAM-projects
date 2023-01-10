8a2142ebbd5af7dc65ad3370648c0b53e0594653
fermarx <48553686+fermarx@users.noreply.github.com>
2023-01-10 12:03:51 +0100

    ADSOF

---
+++ ADSOF/P2/e1/src/prac2/traffic/PollutionIndex.java
@@ -0,0 +1,20 @@
+package prac2.traffic;
+
+/* 
+ * Enumeration that classifies the vehicles taking into account their fabrication year
+ * 
+ * @author Jose Manuel Freire and Sofia Xiaofan Fernandez Marin jose.freire@estudiante.uam.es sofiax.fernandez@estudiante.uam.es
+ * 
+ */
+public enum PollutionIndex {
+	A, B, C;
+	
+	private static final int DATEA = 2018;
+	private static final int DATEB = 2010;
+	
+	public static PollutionIndex getPollutionIndex(int year) {
+		if (year >= DATEA) return A;
+		if (year >= DATEB) return B;
+		return C;
+	}
+}
